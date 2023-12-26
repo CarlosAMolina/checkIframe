ef22417da7f9211ff506882450c676096395da93
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2023-12-26 12:35:04 +0100

    Add test

--- test/popup.test.js
+++ test/popup.test.js
@@ -69,6 +69,10 @@ describe("Check module import", () => {
   it('The module should be imported without errors and has expected values', function() {
     expect(ModulePopup.__get__('urlTypeBlacklist')).toBe('blacklist');
   });
+  it('popupMain runs without error', function() {
+      function_ = ModulePopup.__get__('popupMain');
+      function_();
+  });
   it('initializePopup runs without error', function() {
       function_ = ModulePopup.__get__('initializePopup');
       function_();
