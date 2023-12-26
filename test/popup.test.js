553f3824c7af29e05bac74a2601a9551c2567ef0
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2023-12-26 11:58:51 +0100

    Add test

--- test/popup.test.js
+++ test/popup.test.js
@@ -69,6 +69,10 @@ describe("Check module import", () => {
   it('The module should be imported without errors and has expected values', function() {
     expect(ModulePopup.__get__('urlTypeBlacklist')).toBe('blacklist');
   });
+  it('sendInfoAndValue runs without error', function() {
+      function_ = ModulePopup.__get__('sendInfoAndValue');
+      function_();
+  });
   it('sendInfoSaveAndShowAnswer runs without error', function() {
       console.error = jest.fn();
       function_ = ModulePopup.__get__('sendInfoSaveAndShowAnswer');
