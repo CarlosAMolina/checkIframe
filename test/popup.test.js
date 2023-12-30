d662b1e2d8bf3b1b54951dc49727d1f87a860000
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2023-12-30 15:16:44 +0100

    Add test

--- test/popup.test.js
+++ test/popup.test.js
@@ -168,6 +168,28 @@ describe("Check module import", () => {
         // TODO check and control lastCall[1].values (is affected by other tests that create a big array of aleatory size).
       });
     });
+    describe("Check ButtonClean", () => {
+      beforeAll(() => {
+        const classType = popupModule.__get__("ButtonClean");
+        button = new classType();
+      });
+      it("Check it has correct button ID value", function () {
+        expect(button.buttonIdHtml).toBe("buttonClean");
+      });
+      it("Check run has expected calls and values", async () => {
+        document.querySelector("#infoScroll").classList.remove("hidden");
+        expect(document.getElementById("infoScroll").className).toBe("");
+        await button.run;
+        const buttonIdHtml = "buttonClean";
+        expect(popupModule.__get__("info2sendFromPopup")).toBe(buttonIdHtml);
+        expect(document.getElementById("infoScroll").className).toBe("hidden");
+        expect(browser.tabs.sendMessage.mock.calls.length).toBe(1);
+        const lastCall = browser.tabs.sendMessage.mock.lastCall;
+        expect(lastCall[0]).toBe(tabId);
+        expect(lastCall[1].info).toBe(buttonIdHtml);
+        // TODO check and control lastCall[1].values (is affected by other tests that create a big array of aleatory size).
+      });
+    });
   });
   describe("Check getShowLogs", () => {
     beforeEach(() => {
