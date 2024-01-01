232906bb437257d51e80e743dfb3a5529db7fbdb
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2024-01-01 17:12:49 +0100

    Fix test

--- test/popup.test.js
+++ test/popup.test.js
@@ -514,7 +514,7 @@ describe("Check module import", () => {
         const eValue = "https://foo.com/test.html";
         function_(eKey, eValue);
         expect(popupModule.__get__("infoContainer").innerHTML).toBe(
-          '<div><div><button title="Delete" class="floatLeft button" style="margin: 0% auto"><img src="/icons/trash.png"></button><p style="margin-left: 45px"></p><div class="clearfix"></div></div><div style="display: none;"><input class="input" style="width:70%"><button title="Update" class="button" style="margin: 0% auto"><img src="/icons/ok.png"></button><button title="Cancel update" class="floatRight button" style="margin: 0% auto"><img src="/icons/cancel.png"></button><div class="clearfix"></div></div></div>',
+          '<div><div><button title="Delete" class="floatLeft button" style="margin: 0% auto"><img src="/icons/trash.png"></button><p style="margin-left: 45px">https://foo.com/test.html</p><div class="clearfix"></div></div><div style="display: none;"><input class="input" style="width:70%"><button title="Update" class="button" style="margin: 0% auto"><img src="/icons/ok.png"></button><button title="Cancel update" class="floatRight button" style="margin: 0% auto"><img src="/icons/cancel.png"></button><div class="clearfix"></div></div></div>',
         );
         console.info("xxx end"); // TODO rm
       });
