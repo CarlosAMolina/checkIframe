ed818f47e6d25378016669572932ce6fbce178ba
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2024-09-07 15:52:27 +0200

    feat(tags): add new lines

--- src/popup/tagsHtml.js
+++ src/popup/tagsHtml.js
@@ -1,14 +1,18 @@
 export function getStrTagsHtml(frameTagSummary, iframeTagSummary) {
     const countDetectedTags = frameTagSummary.sourcesAllNumber + iframeTagSummary.sourcesAllNumber;
     let result = `<p>Total number of frames and iframes: ${countDetectedTags}</p>`;
+    result += '\n';
     result += getTagHtml("frame", frameTagSummary);
+    result += '\n';
     result += getTagHtml("iframe", iframeTagSummary);
     return result;
 }
 
 function getTagHtml(tag, tagSummary) {
     let result = `<p>Total number of ${tag}s: ${tagSummary.sourcesValid.length}</p>`;
+    result += '\n';
     result += `<p>Not blacklisted ${tag}s (${tagSummary.sourcesValid.length}):</p>`;
+    result += '\n';
     result += getUrlsHtml(tagSummary);
     return result
 }
@@ -18,7 +22,7 @@ function getUrlsHtml(tagSummary) {
     for (let index = 0; index < tagSummary.sourcesValid.length; index++) {
       const url = tagSummary.sourcesValid[index];
       const urlHtml = `<a href="${url}">${url}</a>`
-      elements += `<li>${urlHtml}</li>`
+      elements += `\n  <li>${urlHtml}</li>`
     }
-    return `<ol>${elements}</ol>`
+    return `<ol>${elements}\n</ol>`
 }
