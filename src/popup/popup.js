27a42906ed119cc852e9df17117a9e49336ea6c1
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2025-12-07 22:01:17 +0100

    feat(copy button): add js copy url to clipboard

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -481,6 +481,7 @@ function changeParagraph(response, htmlId) {
       const iframeTagSummary = response["iframe"];
       const htmlStr = getStrTagsHtml(frameTagSummary, iframeTagSummary);
       sourcesContainer.insertAdjacentHTML("afterbegin", htmlStr);
+      setupCopyButtonListeners();
     }
   }
 }
@@ -582,6 +583,45 @@ function updateElementsWhenIncompatibleWebPage() {
   for (const element of elementsToHide) {
     document.querySelector(element).classList.add("hidden");
   }
+}
+
+function setupCopyButtonListeners() {
+  const buttons = document.querySelectorAll(".detections button");
+  buttons.forEach((button) => {
+    button.addEventListener("mouseenter", function () {
+      button.classList.toggle("show");
+    });
+    button.addEventListener("mouseleave", function () {
+      button.classList.toggle("show");
+    });
+    button.addEventListener("click", function () {
+      const url = this.parentElement.querySelector("a").href;
+      // Find the anchor tag in the same list item
+      // Copy the URL to the clipboard
+      navigator.clipboard
+        .writeText(url)
+        .then(() => {
+          console.log("Copied to clipboard: " + url);
+          const button = this.parentElement.querySelector("button");
+          const image = button.querySelector("img");
+          const tooltip = button.querySelector("span");
+          const originalText = tooltip.textContent;
+          const originalSrc = image.src;
+          image.src = "ok.svg";
+          tooltip.textContent = "Copied";
+          // Avoid wrong behaviour if the user clicks when the temporal image is displayed.
+          button.disabled = true;
+          setTimeout(() => {
+            image.src = originalSrc;
+            tooltip.textContent = originalText;
+            button.disabled = false;
+          }, 1000);
+        })
+        .catch((err) => {
+          console.error("Failed to copy: ", err);
+        });
+    });
+  });
 }
 
 // when the pop-up loads, inject a content script into the active tab,
