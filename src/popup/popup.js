2359754bd7672ec1281f9ef8aabb10c94211ce47
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2025-12-20 13:27:52 +0100

    refactor(button): replace global variable with parameter

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -380,7 +380,7 @@ function showStoredInfo(eKey, eValue) {
         var searchInStorage = Object.keys(result); // array with the searched value if it is stored
         if (searchInStorage.length < 1) {
           // searchInStorage.length < 1 -> no stored
-          updateEntry(eKey, id2save);
+          updateEntry(eKey, id2save, info2save);
           entry.parentNode.removeChild(entry);
         }
       });
@@ -388,7 +388,7 @@ function showStoredInfo(eKey, eValue) {
   });
 
   // update
-  function updateEntry(id2change, id2save) {
+  function updateEntry(id2change, id2save, info2save) {
     addUrl(id2save);
     // TODO replace [id2save] -> id2save
     var storingInfo = browser.storage.local.set({ [id2save]: info2save });
