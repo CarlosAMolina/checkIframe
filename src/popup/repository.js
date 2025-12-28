0403b22514cb31b9cc1baa2a6b7329809bd08c8c
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2025-12-28 08:00:14 +0100

    refactor(repository): move line

--- src/popup/repository.js
+++ src/popup/repository.js
@@ -3,11 +3,11 @@ export class BrowserRepository {
     this._browser = browser;
   }
 
-  getByKey(key) {
-    return this._browser.storage.local.get(key);
-  }
-
   getAll() {
     return this._browser.storage.local.get(null);
+  }
+
+  getByKey(key) {
+    return this._browser.storage.local.get(key);
   }
 }
