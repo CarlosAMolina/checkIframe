818747b462155adf106a0ac1c84fafbae29b50ae
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2025-12-29 15:01:33 +0100

    refactor(popup): move line

--- src/popup/dom.js
+++ src/popup/dom.js
@@ -2,14 +2,14 @@ export function hide(htmlId) {
   document.querySelector("#" + htmlId).classList.add("hidden");
 }
 
-export function unhide(htmlId) {
-  document.querySelector("#" + htmlId).classList.remove("hidden");
-}
-
 export function hideOrUnhide(htmlId) {
   if (document.getElementById(htmlId).classList.contains("hidden")) {
     unhide(htmlId);
   } else {
     hide(htmlId);
   }
+}
+
+export function unhide(htmlId) {
+  document.querySelector("#" + htmlId).classList.remove("hidden");
 }
