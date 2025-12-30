06d3c682212d88c38577cb996d59ae6929a86f57
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2025-12-30 13:17:37 +0100

    docs(message-mediator): change comment

--- src/popup/message-mediator.js
+++ src/popup/message-mediator.js
@@ -3,7 +3,7 @@ import { updateElementsWhenIncompatibleWebPage } from "./dom.js";
 
 export function sendMessage(message) {
   browser.tabs
-    .query({ active: true, currentWindow: true }) // Send to current tab.
+    .query({ active: true, currentWindow: true }) // Get current tab.
     .then((tabs) => browser.tabs.sendMessage(tabs[0].id, message))
     .catch(onSendInfoError);
 }
