ba6f342cf463bc2907202c3478e4b31bb631d13e
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2025-12-30 13:03:35 +0100

    feat(message-mediator): add new send message function

--- src/popup/message-mediator.js
+++ src/popup/message-mediator.js
@@ -1,6 +1,14 @@
 import { reportError } from "./log.js";
 import { updateElementsWhenIncompatibleWebPage } from "./dom.js";
 
+// TODO drop New and drop old sendMessage
+export function sendMessageNew(message) {
+  browser.tabs
+    .query({ active: true, currentWindow: true }) // Send to current tab.
+    .then((tabs) => browser.tabs.sendMessage(tabs[0].id, message))
+    .catch(reportError); // TODO use onSendInfoError
+}
+
 export function sendMessage(tab, message) {
   browser.tabs.sendMessage(tab.id, message).catch(onSendInfoError);
 }
--- test/popup/message-mediator.test.js
+++ test/popup/message-mediator.test.js
@@ -6,6 +6,19 @@ describe("message-mediator", () => {
   beforeEach(() => {
     global.browser = fakeModule.fakeBrowser();
   });
+  it("sendMessageNew has expected calls and values", async () => {
+    const info2send = "info 2 send";
+    const value2send = "value 2 send";
+    const message = modelModule.Message(info2send, value2send);
+    await messageMediatorModule.sendMessageNew(message);
+    expect(browser.tabs.sendMessage.mock.lastCall).toStrictEqual([
+      1,
+      {
+        info: "info 2 send",
+        values: "value 2 send",
+      },
+    ]);
+  });
   it("sendMessage has expected calls and values", function () {
     // The first time the popup is initialized I think it has these values.
     const tab = { id: 1234 };
