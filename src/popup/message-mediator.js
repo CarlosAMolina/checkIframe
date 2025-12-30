1f818e8f8b054f516370d834b10371ca394cac93
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2025-12-30 03:50:33 +0100

    refactor(check-and-border): extract constant

--- src/popup/message-mediator.js
+++ src/popup/message-mediator.js
@@ -2,12 +2,11 @@ import { reportError } from "./log.js";
 import { updateElementsWhenIncompatibleWebPage } from "./dom.js";
 
 export function sendMessage(tabs, info2sendFromPopup, values2sendFromPopup) {
-  browser.tabs
-    .sendMessage(tabs[0].id, {
-      info: info2sendFromPopup,
-      values: values2sendFromPopup,
-    })
-    .catch(onSendInfoError);
+  const message = {
+    info: info2sendFromPopup,
+    values: values2sendFromPopup,
+  };
+  browser.tabs.sendMessage(tabs[0].id, message).catch(onSendInfoError);
 }
 
 function onSendInfoError(error) {
