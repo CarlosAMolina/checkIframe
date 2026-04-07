82694b55ff6bef3d782e30d8e4047dfb2fc5f296
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2026-04-07 22:23:34 +0200

    refactor: move line

--- src/popup/buttons.js
+++ src/popup/buttons.js
@@ -80,6 +80,21 @@ export function initializePopupButtons() {
   new ButtonAlwaysShowSources().initializePopup();
 }
 
+// TODO? move to another file, it should not be in buttons.js
+// save input box info
+export function saveUrl(enterKey, urlType) {
+  let info2save = document
+    .querySelector('textarea[id="inputUrl"]')
+    .value.split("\n");
+  if (enterKey == 1) {
+    info2save.pop(); // delete last value (\n)
+  }
+  browser.tabs
+    .query({ active: true, currentWindow: true })
+    .then(() => storeInfo(info2save, urlType))
+    .catch(reportError);
+}
+
 class Button {
   click() {
     throw TypeError("Not implemented");
@@ -697,21 +712,6 @@ function showStoredUrlsType(urlType) {
 // TODO deprecate removeShownStoredUrls, use removeChildren only.
 function removeShownStoredUrls(infoContainer) {
   removeChildren(infoContainer);
-}
-
-// TODO? move to another file, it should not be in buttons.js
-// save input box info
-export function saveUrl(enterKey, urlType) {
-  let info2save = document
-    .querySelector('textarea[id="inputUrl"]')
-    .value.split("\n");
-  if (enterKey == 1) {
-    info2save.pop(); // delete last value (\n)
-  }
-  browser.tabs
-    .query({ active: true, currentWindow: true })
-    .then(() => storeInfo(info2save, urlType))
-    .catch(reportError);
 }
 
 // add a tag to the display, and storage
