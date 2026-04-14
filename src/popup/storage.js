8c5eba762eb80d2d22212b57e30692a9b1617cae
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2026-04-14 21:38:30 +0200

    refactor: replace promise all with loop

--- src/popup/storage.js
+++ src/popup/storage.js
@@ -7,25 +7,23 @@ import { showStoredInfo } from "./ui.js";
 
 // add a tag to the display, and storage
 export async function saveUrls(infoContainer, urlsInput, urlType) {
-  urlsInput = [...new Set(urlsInput)];  // delete duplicates
+  urlsInput = [...new Set(urlsInput)]; // delete duplicates
   const repository = new BrowserRepository(browser);
   let urls = getUrls();
-  await Promise.all(
-    urlsInput.map(async function (url) {
-      let urlKey = urlType + "_" + url;
-      try {
-        const isStored = await repository.isStored(urlKey);
-        if (!isStored) {
-          urls = addUrl(urlKey, urls, urlType);
-          setUrls(urls);
-          const message = Message("urls", urls);
-          sendMessage(message);
-          await repository.save(urlKey, url);
-          showStoredInfo(infoContainer, urlKey, url);
-        }
-      } catch (e) {
-        reportError(e);
+  for (const url of urlsInput) {
+    let urlKey = urlType + "_" + url;
+    try {
+      const isStored = await repository.isStored(urlKey);
+      if (!isStored) {
+        urls = addUrl(urlKey, urls, urlType);
+        setUrls(urls);
+        const message = Message("urls", urls);
+        sendMessage(message);
+        await repository.save(urlKey, url);
+        showStoredInfo(infoContainer, urlKey, url);
       }
-    }),
-  );
+    } catch (e) {
+      reportError(e);
+    }
+  }
 }
